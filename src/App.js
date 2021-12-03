b2c067d4bdca941e3acf6518663a47c588045a73
Rahaman <mdshahidur_rahaman1@baylor.edu>
2021-12-03 03:18:57 -0600

    second

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+            <div className="row">
+                <div className="todolist">
+                <h1>Team List</h1>
+                    <br></br>
+                    "teamId": 1, <br></br>
+                    "teamName": "Code Hunter", <br></br>
+                    "teamState": "Accepted", <br></br> <br></br> 
+
+                    "teamId": 2, <br></br>
+                    "teamName": "Baylor 1", <br></br>
+                    "teamState": "Accepted", <br></br> <br></br>
+
+                    "teamId": 3, <br></br>
+                    "teamName": "Baylor 2", <br></br>
+                    "teamState": "Accepted", <br></br>
+                    
+
+                </div>
+            </div>
+        </div>
+  );
+}
+
+export default App;
